c084d1332819f235d12a47a2f1f2ad864fc6aff6
Taqia Tahmida <taqia@graduate.utm.com>
2026-01-18 14:22:50 +0800

    resolve error- 5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         View::composer(['components.navbar', 'components.dashboard-navbar'], function ($view) {
             $unreadAdminMessages = 0;
             $user = Auth::user();
